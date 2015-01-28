9cbc11118a4346697cfb745da2b78550e55fc7ff
Matthew Mayer <matthewkmayer@gmail.com>
2015-01-27 21:00:44 -0800

    Adds golang code samples.

---
+++ bulkheading-lokki/timeout2.go
@@ -0,0 +1,16 @@
+package main
+import "time"
+import "fmt"
+func main() {
+    c2 := make(chan string, 1)
+    go func() { time.Sleep(time.Second * 2)
+        c2 <- "result 2"
+    }()
+    select {
+    case res := <-c2:
+        fmt.Println(res)
+    case <-time.After(time.Second * 3):
+        fmt.Println("timeout 2")
+    }
+}
+
