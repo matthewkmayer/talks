6fd3d7cf6f1947f5eeeb61ee8de1466afedc40cf
Matthew Mayer <matthewkmayer@gmail.com>
2015-03-11 22:18:16 -0700

    Slide revisions, commands cheat sheet and installing golang in Vagrant.

--- strangler-infusereo/code/bootstrap.sh
+++ strangler-infusereo/code/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo apt-get install -y ruby-dev
+sudo apt-get install -y ruby-dev gccgo-go
 sudo gem install sinatra cucumber json rspec
---
+++ strangler-infusereo/commands-cheat-sheet.sh
@@ -0,0 +1,19 @@
+# Demo all ruby code:
+vagrant up
+vagrant ssh
+
+# ruby service up:
+cd /vagrant/code/
+ruby ruby-service.rb &
+
+# cuke tests:
+cucumber
+
+
+# Demoing strangler:
+# Compile golang service:
+
+# run them both:
+
+# cucumber tests still work:
+cucumber
